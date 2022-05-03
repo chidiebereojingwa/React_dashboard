a81310547b15633cfc97a263397fd052bc90fd74
chidiebereojingwa <ojingwaoji@gmail.com>
2022-05-03 17:00:59 +0100

    Add Data.js

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
-import Logo from "../../imgs/logo.png"
-import './Sidebar.css'
+import React from "react";
+import Logo from "../../imgs/logo.png";
+import "./Sidebar.css";
+import { UilEstate } from "@iconscout/react-unicons";
 export default function Sidebar() {
   return (
-    <div className='Sidebar'>
-        {/* logo */}
-        <div className='logo'>
-            <img src={Logo} alt=''/>
-            <span>
-                Sh<span>o</span>ps
-            </span>
+    <div className="Sidebar">
+      {/* logo */}
+      <div className="logo">
+        <img src={Logo} alt="" />
+        <span>
+          Sh<span>o</span>ps
+        </span>
+      </div>
+
+      {/* menu */}
+      <div className="menu">
+        <div className="menuItem">
+          <div>
+            <UilEstate />
+          </div>
+          <span>Dashboard</span>
         </div>
 
-        {/* menu */}
-        <div className='menu'>
-            <div className='menuItem'>
-                <div>
-                    Icon
-                </div>
-                <span>
-                    Dashboard
-                </span>
-                </div>
+        <div className="menuItem">
+          <div>
+            <UilEstate />
+          </div>
+          <span>Dashboard</span>
         </div>
 
+        <div className="menuItem">
+          <div>
+            <UilEstate />
+          </div>
+          <span>Dashboard</span>
+        </div>
+
+        <div className="menuItem">
+          <div>
+            <UilEstate />
+          </div>
+          <span>Dashboard</span>
+        </div>
+
+        <div className="menuItem">
+          <div>
+            <UilEstate />
+          </div>
+          <span>Dashboard</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
