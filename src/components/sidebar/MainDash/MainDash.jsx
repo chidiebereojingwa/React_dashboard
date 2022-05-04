fd1693d9e1546b3b997994719bf039ba5b438a09
chidiebereojingwa <ojingwaoji@gmail.com>
2022-05-04 22:45:33 +0100

    add card component

--- src/components/sidebar/MainDash/MainDash.jsx
+++ src/components/sidebar/MainDash/MainDash.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./MainDash.css"
+import Cards from '../../Cards/Cards';
 const MainDash = () => {
   return (
     <div className='MainDash'>
         <h1>Dashboard</h1>    
-        <Card/> 
+        <Cards/> 
     </div>
   );
 }
