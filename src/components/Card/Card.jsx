6805b419a93979eea6f1b67b0d3679e71c230531
chidiebereojingwa <ojingwaoji@gmail.com>
2022-05-06 22:19:45 +0100

    Add expanded card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { AnimateSharedLayout } from 'framer-motion';
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import "./Card.css"
 
 const Card = (props) => {
   const [expanded, setExpanded] = useState(false)
@@ -11,31 +12,42 @@ const Card = (props) => {
     <AnimateSharedLayout>
       {
         expanded?(
-          "ExpandedCard"
-          // <ExpandedCard/>
-        ):
-        <CompactCard param={props}/>
+         <ExpandedCard param={props} setExpanded={()=>setExpanded(false)}/>
+        ): 
+        <CompactCard param={props} setExpanded={()=> setExpanded(true)}/>
       }
     </AnimateSharedLayout>
   ); 
 }
 
 
+//  "ExpandedCard"
 
-// CompactCard
-function CompactCard ({param}){
+function ExpandedCard ({param, setExpanded}){
+  return (
+    <div className='ExpandedCard'>
+      
+    </div>
+  )
+}
+
+// CompactCard "ExpandedCard"
+function CompactCard ({param, setExpanded}){
   const Png = param.png
   return (
     <div className="CompactCard"
     style={{
       background : param.color.backGround,
       boxShadow : param.color.boxShadow
-    }}>
+    }}
+    onClick={setExpanded}
+    >
       <div className="radialBar">
         <CircularProgressbar
         value={param.barValue}
         text={`${param.barValue}%`}
         />
+        <span>{param.title}</span>
       </div>
       <div className="detail">
         <Png />
